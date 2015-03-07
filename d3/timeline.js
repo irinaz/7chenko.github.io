4a2789ab427709979f0837f7b2a747ed9aae9aec
misha <misha@MacBook-Pro.local>
2015-03-06 21:52:11 -0800

    without toYear

--- d3/timeline.js
+++ d3/timeline.js
@@ -390,7 +390,7 @@ function timeline(domElement) {
             .scale(band.xScale)
             .orient(orientation || "bottom")
             .tickSize(6, 0)
-            .tickFormat(function (d) { return toYear(d); });
+            .tickFormat(function (d) { return d; /*toYear(d);*/ });
 
         var xAxis = chart.append("g")
             .attr("class", "axis")
